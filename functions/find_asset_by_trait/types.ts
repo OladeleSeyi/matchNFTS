5a47ff2831ff2017d2178804eca42df8d0137687
oladeleseyi <toothy61@gmail.com>
2022-09-05 00:25:02 +0100

    more types

--- functions/find_asset_by_trait/types.ts
+++ functions/find_asset_by_trait/types.ts
@@ -1,5 +1,5 @@
 type genericObject = {
-  [key: string]: string | genericObject;
+  [key: string]: string | string[] | genericObject[] | genericObject;
 };
 
 type traitType = {
@@ -8,15 +8,23 @@ type traitType = {
 };
 
 export interface AlchemyResponseType {
-  ownedNfts: OwnedNftType<genericObject>[];
+  ownedNfts: OwnedNftType[];
   totalCount: Number;
   pageKey: string | null;
   blockHash: string;
 }
 
-export interface OwnedNftType<T> {
+export interface OwnedNftType extends genericObject {
   metadata: {
     image: string;
     attributes: traitType[];
+  };
+}
+export interface RequestSchema {
+  chain: string;
+  contract: string;
+  owner: string;
+  traits: {
+    [key: string]: string[];
   };
 }
